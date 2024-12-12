bc7a94e482a6913e82d54a4b24d2f7e29e6b35d4
Noviprofil188 <noviprofil188@gmail.com>
2024-12-12 10:41:46 +0100

    Kreiranje stranica i ruta

--- Frontend/quasar-project/src/layouts/MainLayout.vue
+++ Frontend/quasar-project/src/layouts/MainLayout.vue
@@ -12,7 +12,7 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          Knjiznica projekt
         </q-toolbar-title>
 
         <div>Quasar v{{ $q.version }}</div>
@@ -55,46 +55,46 @@ defineOptions({
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: 'Login',
+    caption: 'Stranica za log in',
+    icon: '',
+    link: '#/'
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: 'Lokacija',
+    caption: 'Lokacija',
+    icon: '',
+    link: '#/'
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: 'Mapa',
+    caption: 'Mapa knjiga',
+    icon: '',
+    link: '#/'
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    title: 'Onama',
+    caption: 'O nama',
+    icon: '',
+    link: '#/'
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    title: 'Popis knjiga',
+    caption: 'Popis svih knjiga',
+    icon: '',
+    link: '#/'
   },
   {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
+    title: 'Registracija',
+    caption: 'Registracija',
+    icon: '',
+    link: '#/'
   },
   {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    title: 'Traži knjigu',
+    caption: 'Pretraži knjigu',
+    icon: '',
+    link: '#/'
   }
 ]
 
--- Frontend/quasar-project/src/router/routes.js
+++ Frontend/quasar-project/src/router/routes.js
@@ -3,7 +3,14 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
+      { path: '', component: () => import('pages/IndexPage.vue') },
+      { path: 'Login', component: () => import('pages/LoginPage.vue') },
+      { path: 'Lokacija', component: () => import('pages/LokacijaPage.vue') },
+      { path: 'Mapa', component: () => import('pages/MapaPage.vue') },
+      { path: 'Onama', component: () => import('pages/OnamaPage.vue') },
+      { path: 'PopisKnjiga', component: () => import('pages/PopisKnjigaPage.vue') },
+      { path: 'Registracija', component: () => import('pages/RegistracijaPage.vue') },
+      { path: 'Trazi knjigu', component: () => import('pages/TraziKnjiguPage.vue') },
     ]
   },
 
