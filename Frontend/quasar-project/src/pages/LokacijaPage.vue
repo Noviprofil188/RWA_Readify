0d4bf0d490f6407b2f80dc5c25016422fcbba2bf
Noviprofil188 <noviprofil188@gmail.com>
2024-12-12 12:07:27 +0100

    Rjesavanje stranice Lokacija, dodavanje karte i uredivanje

--- Frontend/quasar-project/src/pages/LokacijaPage.vue
+++ Frontend/quasar-project/src/pages/LokacijaPage.vue
@@ -1,11 +1,40 @@
 <template>
   <q-page padding>
-    <!-- content -->
+    <div id="map" style="height: 500px;"></div>
   </q-page>
 </template>
 
 <script>
+import { ref, onMounted } from 'vue';
+import * as L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
 export default {
-  // name: 'PageName',
-}
+  setup() {
+    const initialMap = ref(null); // Inicijalizacija reference za kartu
+
+    onMounted(() => {
+      // Kreiranje karte na elementu s ID-jem 'map'
+      initialMap.value = L.map('map').setView([45.3312, 14.4322], 13);
+
+      // Dodavanje tile layer-a (ploča) na kartu
+      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }).addTo(initialMap.value);
+    });
+
+    return {
+      initialMap,
+    };
+  },
+};
 </script>
+
+<style scoped>
+/* Dodavanje osnovnog stila za mapu */
+#map {
+  width: 100%;
+  height: 500px; /* Visina mape definirana u inline stilu unutar <template> */
+}
+</style>
